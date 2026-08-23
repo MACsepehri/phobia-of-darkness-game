30aaa5c98c49cb7dfac618926e240dda268ecbcd
MACsepehri <macsepehri@gmail.com>
2026-08-23 10:02:33 +0330

    Added run function.

--- Engine/main.py
+++ Engine/main.py
@@ -1,6 +1,25 @@
 import pygame
+import sys
 
 pygame.init()
 
 class Engine:
-    pass
+    def __init__(self, title="Game", fullscreen=(False, (400, 400)), icon=""):
+        self.win = None
+        self.size = fullscreen[1]
+        self.fullscreen = fullscreen
+        self.icon = icon
+        self.win = pygame.display.set_mode(self.size)
+        if self.icon != "": pygame.display.set_icon(pygame.image.load(self.icon))
+
+    def run(self, update_func = lambda: ..., event_func = lambda: ..., exit_func = lambda: ...):
+        while True:
+            for self.event in pygame.event.get():
+                if self.event.type == pygame.QUIT:
+                    exit_func()
+                    sys.exit()
+                event_func()
+
+
+            update_func()
+            pygame.display.update()
